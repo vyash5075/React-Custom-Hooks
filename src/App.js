c4cc550477c2855367ba57279e56c3983572b0f6
Yash Verma <yashv@lambdatest.com>
2022-07-05 01:26:49 +0530

    update Effect hook

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import ToggleComponent from  '../src/Toggle/ToggleComponent'
 import TimeOutComponent from '../src/useTimout/TimeCompoent'
 import DebounceComponent from '../src/useDebounce/DebounceComponent'
 import UpdateEffect from '../src/useUpdateEffect/UpdateEffectComponent'
+import UseArray from '../src/useArray/ArrayComponent'
 function App() {
   return (
     <div className="App">
-      {/* <ToggleComponent/>
-      <TimeOutComponent/> */}
-      {/* <DebounceComponent/> */}
-      <UpdateEffect/>
+      <ToggleComponent/>
+      <TimeOutComponent/> 
+      <DebounceComponent/>
      
+      <UpdateEffect/>
+     <UseArray/>
     </div>
   );
 }
