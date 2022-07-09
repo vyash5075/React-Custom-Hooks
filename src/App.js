b217d38d2953de31986305ed72d779bb530c7dde
Yash Verma <yashv@lambdatest.com>
2022-07-09 17:58:18 +0530

    add usemediaquery hook

--- src/App.js
+++ src/App.js
@@ -1,33 +1,38 @@
-import './App.css';
-import ToggleComponent from  '../src/Toggle/ToggleComponent'
-import TimeOutComponent from '../src/useTimout/TimeCompoent'
-import DebounceComponent from '../src/useDebounce/DebounceComponent'
-import UpdateEffect from '../src/useUpdateEffect/UpdateEffectComponent'
-import UseArray from '../src/useArray/ArrayComponent'
-import UsePrevious from '../src/usePrevious/PreviousComponent'
-import UseStateWithHistory from '../src/useStateWithHistory/StateWithHistoryComponent'
-import UseStorage from '../src/useStorage/StoageComponent'
-import UseAsync from '../src/Use-Async/AsyncComponent'
-import UseFetch from '../src/useFetch/FetchComponent'
-import UseScript from '../src/useScript/ScriptComponent'
-import UseEventListener from '../src/useEventListener/EventListenerComponent'
-import UseOnScreen from '../src/useOnScreen/OnScreenComponent'
+import "./App.css";
+import UseToggle from "../src/Toggle/ToggleComponent";
+import useTimeout from "../src/useTimout/TimeCompoent";
+import UseDebounce from "../src/useDebounce/DebounceComponent";
+import UseUpdateEffect from "../src/useUpdateEffect/UpdateEffectComponent";
+import UseArray from "../src/useArray/ArrayComponent";
+import UsePrevious from "../src/usePrevious/PreviousComponent";
+import UseStateWithHistory from "../src/useStateWithHistory/StateWithHistoryComponent";
+import UseStorage from "../src/useStorage/StoageComponent";
+import UseAsync from "../src/Use-Async/AsyncComponent";
+import UseFetch from "../src/useFetch/FetchComponent";
+import UseScript from "../src/useScript/ScriptComponent";
+import UseEventListener from "../src/useEventListener/EventListenerComponent";
+import UseOnScreen from "../src/useOnScreen/OnScreenComponent";
+import UseWindowSize from "../src/usWindowSize/WindowSizeComponent";
+import UseDeepComapareeffect from '../src/useDeepCompareEffect/DeepCompareEffectComponent'
+
 function App() {
   return (
     <div className="App">
-      <ToggleComponent/>
-      <TimeOutComponent/> 
-      <DebounceComponent/>
-      <UpdateEffect/>
-     <UseArray/>
-     <UsePrevious/>
-     <UseStateWithHistory/>
-     <UseStorage/>
-     <UseAsync/>
-     <UseFetch/>
-     <UseScript/>
-     <UseEventListener/>
-     <UseOnScreen/>
+      <UseToggle />
+      <useTimeout />
+      <UseDebounce />
+      <UseUpdateEffect />
+      <UseArray />
+      <UsePrevious />
+      <UseStateWithHistory />
+      <UseStorage />
+      <UseAsync />
+      <UseFetch />
+      <UseScript />
+      <UseEventListener />
+      <UseOnScreen />
+      <UseWindowSize />
+      <UseDeepComapareeffect/>
     </div>
   );
 }
--- src/useDeepCompareEffect/DeepCompareEffectComponent.js
+++ src/useDeepCompareEffect/DeepCompareEffectComponent.js
@@ -40,4 +40,4 @@ export default function DeepCompareEffectComponent() {
     </div>
   )
 }
-Footer
+
